84ec6b814a583a95cd5a1ce26dd86fd2899e7ff6
Will Townsend <will@townsend.io>
2018-09-13 18:21:22 -0700

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,7 +91,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/gatsby-icon.png`,
+        icon: `src/assets/favicon.png`,
       },
     },
     `gatsby-plugin-offline`,
