b9c4fbd440e00c04b4c1c8698180d47eb6412143
Will Townsend <will@townsend.io>
2018-09-19 21:33:38 -0700

    Fix rss feed generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const mdxFeed = require('./src/utils/feed');
+
 module.exports = {
   siteMetadata: {
     title: 'Will Townsend | iOS Developer',
@@ -70,53 +72,53 @@ module.exports = {
       }
     },
 
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              // Class prefix for <pre> tags containing syntax highlighting;
-              // defaults to 'language-' (eg <pre class="language-js">).
-              // If your site loads Prism into the browser at runtime,
-              // (eg for use with libraries like react-live),
-              // you may use this to prevent Prism from re-processing syntax.
-              // This is an uncommon use-case though;
-              // If you're unsure, it's best to use the default value.
-              classPrefix: "language-",
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
-              inlineCodeMarker: null,
-              // This lets you set up language aliases.  For example,
-              // setting this to '{ sh: "bash" }' will let you use
-              // the language "sh" which will highlight using the
-              // bash highlighter.
-              aliases: {},
-            },
-          },
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-          "gatsby-remark-embed-youtube",
-        ],
-      },
-    },
+    // {
+    //   resolve: `gatsby-transformer-remark`,
+    //   options: {
+    //     plugins: [
+    //       {
+    //         resolve: `gatsby-remark-images`,
+    //         options: {
+    //           maxWidth: 590,
+    //         },
+    //       },
+    //       {
+    //         resolve: `gatsby-remark-responsive-iframe`,
+    //         options: {
+    //           wrapperStyle: `margin-bottom: 1.0725rem`,
+    //         },
+    //       },
+    //       {
+    //         resolve: `gatsby-remark-prismjs`,
+    //         options: {
+    //           // Class prefix for <pre> tags containing syntax highlighting;
+    //           // defaults to 'language-' (eg <pre class="language-js">).
+    //           // If your site loads Prism into the browser at runtime,
+    //           // (eg for use with libraries like react-live),
+    //           // you may use this to prevent Prism from re-processing syntax.
+    //           // This is an uncommon use-case though;
+    //           // If you're unsure, it's best to use the default value.
+    //           classPrefix: "language-",
+    //           // This is used to allow setting a language for inline code
+    //           // (i.e. single backticks) by creating a separator.
+    //           // This separator is a string and will do no white-space
+    //           // stripping.
+    //           // A suggested value for English speakers is the non-ascii
+    //           // character '›'.
+    //           inlineCodeMarker: null,
+    //           // This lets you set up language aliases.  For example,
+    //           // setting this to '{ sh: "bash" }' will let you use
+    //           // the language "sh" which will highlight using the
+    //           // bash highlighter.
+    //           aliases: {},
+    //         },
+    //       },
+    //       'gatsby-remark-copy-linked-files',
+    //       'gatsby-remark-smartypants',
+    //       "gatsby-remark-embed-youtube",
+    //     ],
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -144,7 +146,11 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-sitemap`
-    }
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: mdxFeed
+    },
     // {
     //   resolve: 'gatsby-plugin-typography',
     //   options: {
