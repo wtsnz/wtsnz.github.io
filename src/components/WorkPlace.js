9befa677f77931dd53b62e90f0f2f84b3cd9b97e
Will Townsend <will@townsend.io>
2020-09-09 16:46:55 -0700

    Update about

--- src/components/WorkPlace.js
+++ src/components/WorkPlace.js
@@ -4,28 +4,27 @@ import Link from './Link'
 
 const WorkPlaceDiv = ({ link, date, name, image, role, children }) => {
     return (
-            <div className='columns'>
-                    <div className='column is-narrow work-place-image'>
-                        <Link to={link} >
-                            <img style={{ width: '100px', height: '100px', borderRadius: '25px'}} src={image} />
-                        </Link>
-                    </div>
-                    <div className='column'>
-                        <Link className='is-flex' to={link} >
-                            <p className='work-place-name'>{name}</p>
-                        </Link>
-                        <p className='work-place-date'>{date}</p>
-                        <p className='work-place-role'>{role}</p>
-                        <div className='work-place-description'>
-                            {children}
-                        </div>
-                    </div>
+        <div className='columns'>
+            <div className='column is-narrow work-place-image'>
+                <Link to={link} >
+                    <img style={{ width: '100px', height: '100px', borderRadius: '25px' }} src={image} />
+                </Link>
             </div>
+            <div className='column'>
+                <Link className='is-flex' to={link} >
+                    <p className='work-place-name'>{name}</p>
+                </Link>
+                <p className='work-place-date'>{date}</p>
+                <p className='work-place-role'>{role}</p>
+                <div className='work-place-description'>
+                    {children}
+                </div>
+            </div>
+        </div>
     )
 }
 
 const WorkPlace = ({ link, date, name, image, role, children }) => {
-    
     return (
         <div className='work-place'>
             <WorkPlaceDiv link={link} date={date} name={name} image={image} role={role}>
@@ -33,7 +32,6 @@ const WorkPlace = ({ link, date, name, image, role, children }) => {
             </WorkPlaceDiv>
         </div>
     )
-
 }
 
 export default WorkPlace
