cf1e19c8a3233e66807254ab4be9eb539b75fe8d
Will Townsend <will@townsend.io>
2021-01-20 11:54:28 -0800

    Fix project link style when there is no link

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,7 +8,7 @@ const Project = ({ title, subtitle, image, description, link}) => {
     if (link == null) {
         return (
             <div className='flex px-4 -mx-4 space-x-4 py-4 rounded-lg'>
-            <img width="100" height="100" src={image} className="rounded-3xl w-24 h-24"/>
+            <img width="80" height="80" src={image} className="rounded-2xl w-16 h-16"/>
             <div className='flex-1'>
                 <div className="text-xl font-semibold">{title}</div>
                 <div className='font-medium text-gray-500'>{subtitle}</div>
