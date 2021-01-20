c20be5447de71675a672e9c3452286ad4e61699e
Will Townsend <will@townsend.io>
2021-01-20 11:42:27 -0800

    Fix rounded edges on mobile

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const BlogList = ({ postTitle, postSlug, postDate }) => (
-    <Link className='flex px-4 -mx-4 py-2 rounded-md font-medium hover:text-orange-400 hover:bg-orange-50' to={"/" + postSlug}>
+    <Link className='flex px-4 -mx-4 py-2 md:rounded-md font-medium hover:text-orange-400 hover:bg-orange-50' to={"/" + postSlug}>
         <div class="flex-1">
             {postTitle}
         </div>
